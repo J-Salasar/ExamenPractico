cb20ac63e7a82e5c217b6673b357f7875b3cb30e
Salasar001 <salasar1820@gmail.com>
2022-10-22 21:23:06 -0600

    Primera codificacion

--- app/src/main/java/com/example/examenpractico/MainActivity.java
+++ app/src/main/java/com/example/examenpractico/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.examenpractico;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-private String nombre="";
+private ImageView imagen;
+private EditText e_nombre,e_numero,e_nota;
+private Spinner p_spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imagen=(ImageView) findViewById(R.id.foto);
+        e_nombre=(EditText) findViewById(R.id.txtnombre);
+        e_numero=(EditText) findViewById(R.id.txtnumero);
+        e_nota=(EditText) findViewById(R.id.txtnota);
+        p_spinner=(Spinner) findViewById(R.id.spinner_pais);
     }
 }
