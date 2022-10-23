4d6b58c0954031e36e7e05821f5825d021c832c3
Salasar001 <salasar1820@gmail.com>
2022-10-22 22:16:03 -0600

    Fecha: 22/10/22/22:15

--- app/src/main/java/com/example/examenpractico/MainActivity.java
+++ app/src/main/java/com/example/examenpractico/MainActivity.java
@@ -1,19 +1,42 @@
 package com.example.examenpractico;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 private ImageView imagen;
 private EditText e_nombre,e_numero,e_nota;
 private Spinner p_spinner;
+private String currentPhotoPath;
+private Button guardar;
+private static final int REQUESTCODECAMARA=100;
+private static final int REQUESTTAKEFOTO=101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +46,109 @@ private Spinner p_spinner;
         e_numero=(EditText) findViewById(R.id.txtnumero);
         e_nota=(EditText) findViewById(R.id.txtnota);
         p_spinner=(Spinner) findViewById(R.id.spinner_pais);
+        guardar=(Button) findViewById(R.id.salvar);
         String[] opciones = {"Seleccione", "Sumar", "Restar", "Multiplicar", "Dividir"};
         ArrayAdapter<String> adactador = new ArrayAdapter<String>(this, R.layout.item_spinner, opciones);
         p_spinner.setAdapter(adactador);
+        imagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                permisos();
+            }
+        });
+    }
+    public void validar(View view){
+        Toast.makeText(this,currentPhotoPath,Toast.LENGTH_LONG).show();
+        if((e_nombre.getText().toString()) != null){
+            if((e_numero.getText().toString()) != null){
+                if((e_nota.getText().toString()) != null){
+                    if(currentPhotoPath != null){
+
+                    }
+                    else{
+
+                    }
+                }
+                else{
+
+                }
+            }
+            else{
+
+            }
+        }
+        else{
+
+        }
+    }
+    private void galleryAddPic(){
+        Intent mediaScanIntent=new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f=new File(currentPhotoPath);
+        Uri contenUri=Uri.fromFile(f);
+        mediaScanIntent.setData(contenUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+    private void dispatchTakePictureIntent(){
+        Intent takePictureIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager())!=null){
+            File photoFile=null;
+            try {
+                photoFile=createImageFile();
+            }
+            catch (IOException ex){
+            }
+            if(photoFile!=null){
+                Uri photoURI= FileProvider.getUriForFile(this,"com.example.examenpractico.fileprovider",photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,photoURI);
+                startActivityForResult(takePictureIntent,REQUESTTAKEFOTO);
+            }
+        }
+    }
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        //File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName, /* prefix */
+                ".jpg", /* suffix */
+                storageDir /* directory */
+        );
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+    @Override
+    //Agrega la foto al cuadro
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==REQUESTTAKEFOTO && resultCode==RESULT_OK){
+            File foto=new File(currentPhotoPath);
+            imagen.setImageURI(Uri.fromFile(foto));
+            guardar.setEnabled(true);
+            galleryAddPic();
+        }
+    }
+
+    private void permisos(){
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},REQUESTCODECAMARA);
+        }
+        else{
+            dispatchTakePictureIntent();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==REQUESTCODECAMARA){
+            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                dispatchTakePictureIntent();
+            }
+            else{
+                Toast.makeText(getApplicationContext(),"Permiso Denegado",Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
